3c744ad3805332798cecae94c2b52069d8f63064
sukant43 <sukant98657@gmail.com>
2025-02-28 11:59:27 +0530

    seller Login finished

--- backend/controller/sellerLoginController.js
+++ backend/controller/sellerLoginController.js
@@ -53,18 +53,47 @@ const login=async(req,res)=>{
     }
 }
 
-const me=(req,res)=>{
+const me=async(req,res)=>{
     try{
-
+        const{id,name,email,password}=await sellerLogin.findById(req.user.id)
+        return res.status(200).json({
+            id,
+            name,
+            email,
+            password
+        })
     }
     catch(e){
-        return res.status(400).json({error:e})
+        return res.status(400).json({error:e.message})
     }
 }
 
-const changePassword=(req,res)=>{
+const changePassword=async(req,res)=>{
     try{
+        const{email,password,newPassword}=req.body
+        if(!email||!password||!newPassword){
+           return res.status(200).json({msg:"please enter all fields"})
+        }
+        const findUser=await sellerLogin.findOne({email})
+        if (!findUser) {
+            return res.status(404).json({ msg: "User not found" });
+        }
+        if(findUser.id!=req.user.id){
+           return res.status(200).json({msg:"the jsonwebtoken not matches to the email"})
+        }
+        const passCheck=await bcrypt.compare(password,findUser.password)
+        if(!passCheck){
+           return res.status(200).json({msg:"password is not correct"})
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(newPassword, salt);
+        const updatedUser=await sellerLogin.findByIdAndUpdate(findUser.id,
+             { password: hashPassword },
+            { new: true }
+)
+return res.status(200).json({ msg: "Password updated successfully", updatedUser });
 
+        
     }
     catch(e){
         return res.status(400).json({error:e})
--- backend/routes/sellerLoginRoute.js
+++ backend/routes/sellerLoginRoute.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const router=express.Router()
 const{register,login,me,changePassword}=require('../controller/sellerLoginController')
+const {protect}=require('../middleWare/sellerLoginMIddleWare')
 router.post('/register',register)
 
 router.post('/login',login)
 
-router.get('/me',me)
+router.get('/me',protect,me)
 
-router.put('/c-password',changePassword)
+router.put('/c-password',protect,changePassword)
 
 module.exports=router
